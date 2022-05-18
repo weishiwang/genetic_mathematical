b0e9bc62dffe43a68f39571e3e12e4645b897a34
wutangjiabing <2747762657@qq.com>
2022-05-18 20:10:31 +0800

    完成了第一代种群的生成以及适应度的计算

---
+++ 生成数据并保存.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 18 14:19:35 2022
+
+@author: 01
+"""
+
+import xlwt
+import random
+import pandas as pd
+book = xlwt.Workbook(encoding='utf-8',style_compression=0)
+
+	
+sheet = book.add_sheet('遗传算法实现数据',cell_overwrite_ok=True)
+
+col = ('x1', 'x2', 'x3','y')
+for i in range(0,4):
+    sheet.write(0,i,col[i])
+    
+#sheet.write(i+1,j,moneys[i])
+#print("添加成功%S",moneys[i],j,i)
+ls=[[],[],[],[]]
+
+for i in range(200):
+    x1=random.randint(1,30)
+    x2=random.randint(1,30)
+    x3=random.randint(1,30)
+    ls[0].append(x1)
+    ls[1].append(x2)
+    ls[2].append(x3)
+    y=x1*x1+x2-x3*x3
+    ls[3].append(y)
+for i in range(4):
+    for j in range(200):
+        sheet.write(j+1,i,ls[i][j])
+savepath = 'data.xls'
+book.save(savepath)
